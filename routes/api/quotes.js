7fbdd494e4a5fac4840221afed71ce8a14bd42be
Alejandro Ruiz <kannder.ziur@gmail.com>
2021-08-29 20:01:48 -0500

    Se completa el curso. Pendiente agregar los method to delete, update, get and get by ID for authors and Categories.

--- routes/api/quotes.js
+++ routes/api/quotes.js
@@ -8,7 +8,9 @@ const Category = require("../../models/Category");
 
 const getAuthor = require("../../middleware/getAuthor");
 const getCategory = require("../../middleware/getCategory");
+const { route } = require("./authors");
 
+//Create new Quote
 router.post("/", getAuthor, getCategory, async (req, res) => {
   try {
     const quote = new Quote({
@@ -21,6 +23,66 @@ router.post("/", getAuthor, getCategory, async (req, res) => {
     res.status(201).json(newQuote);
   } catch (err) {
     res.status(400).json(err.message);
+  }
+});
+
+//Read all quotes
+router.get("/", async (req, res) => {
+  try {
+    const allQuotes = [];
+    const quotes = await Quote.find();
+    for (quote of quotes) {
+      const author = await Author.findById(quote.author_id);
+      const category = await Category.findById(quote.category_id);
+      allQuotes.push({ quote, author, category });
+    }
+    res.json(allQuotes);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+//Read quote by ID
+
+router.get("/:id", async (req, res) => {
+  try {
+    const quote = await Quote.findById(req.params.id);
+    if (!quote) return res.status(404).json({ message: "Quote not found." });
+    const myQuote = [];
+    const author = await Author.findById(quote.author_id);
+    const category = await Category.findById(quote.category_id);
+    myQuote.push({ quote, author, category });
+    res.json(myQuote);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+//Update quote by Id.
+router.put("/:id", getAuthor, getCategory, async (req, res) => {
+  try {
+    const quote = await Quote.findById(req.params.id);
+    if (!quote) return res.status(404).json({ message: "Quote not found." });
+    if (req.body.quote) quote.quote = req.body.quote;
+    quote.author_id = res.author_id;
+    quote.category_id = res.category_id;
+    const updatedQuote = await quote.save();
+    res.status(201).json(updatedQuote);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+//Delete quote by Id:
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const quote = await Quote.findById(req.params.id);
+    if (!quote) return res.status(404).json({ message: "Quote not found" });
+    await quote.remove();
+    res.json({ message: "Quote remove." });
+  } catch (err) {
+    res.status(500).send(err.message);
   }
 });
 
